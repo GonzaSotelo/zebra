6b4af765a446a6217a3501aa785047ff11d025b0
GonzaSotelo <g.sotelo.dab@gmail.com>
2024-11-05 23:43:48 -0300

    video banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,7 +4,7 @@ import './Banner.css'; // Archivo CSS personalizado para estilos del banner
 const Banner = () => {
   return (
     <div className="banner-container">
-      <video className="banner-video" src="public/video/ZebraLagoEventos.mp4" autoPlay muted loop></video>
+      <video className="banner-video" src="video/ZebraLagoEventos.mp4" autoPlay muted loop></video>
       <div className="banner-overlay">
         
         <p>Vos lo soñas... Nosotros lo hacemos realidad</p>
